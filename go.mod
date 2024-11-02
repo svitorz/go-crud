6402cbdf1a9f9e4dec7f7bc7cb783143647f6b7a
Vitor Souza <vitor.fabio.castro@gmail.com>
2024-11-02 12:46:07 -0300

    add database connection

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
-module svitorz/go-crud
+module github.com/svitorz/go-crud
 
 go 1.23.2
+
+require github.com/joho/godotenv v1.5.1
+
+require github.com/lib/pq v1.10.9 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,59 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+)
+
+func getEnv(key string) string {
+	// Carrega as variáveis do .env
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Erro ao carregar o arquivo .env")
+	}
+	return os.Getenv(key)
+}
+
+func databaseConnection() (*sql.DB, error) {
+	dbUser := getEnv("user")
+	dbName := getEnv("database")
+	dbPass := getEnv("password")
+	dbPort := getEnv("port")
+	sslMode := getEnv("sslmode")
+	dbHost := getEnv("host")
+	dbDriver := getEnv("driver")
+
+	// Monta a string de conexão
+	connStr := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=%s",
+		dbUser, dbPass, dbHost, dbPort, dbName, sslMode,
+	)
+
+	// Abre a conexão com o banco
+	db, err := sql.Open(dbDriver, connStr)
+	if err != nil {
+		return nil, fmt.Errorf("erro ao abrir conexão: %w", err)
+	}
+
+	// Testa a conexão
+	if err = db.Ping(); err != nil {
+		db.Close()
+		return nil, fmt.Errorf("erro ao testar conexão: %w", err)
+	}
+
+	return db, nil
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	db, err := databaseConnection()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	fmt.Println("Conexão com o banco de dados bem-sucedida!")
 }
